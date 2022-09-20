d32def55690d91dece4c683ad7413e60db287df0
Mzazi25 <langat.caleb25@gmail.com>
2022-09-20 07:57:35 +0300

    Added TopRated dataclass

---
+++ app/src/main/java/com/example/chillmax/domain/models/TopRatedMovies.kt
@@ -0,0 +1,22 @@
+package com.example.chillmax.domain.models
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class TopRatedMovies(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val genre_ids: List<Int>,
+    val id: Int,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
+)
