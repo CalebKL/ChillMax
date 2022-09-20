bf73f51f531492592ed6f504efd94804eb94dd7e
Mzazi25 <langat.caleb25@gmail.com>
2022-09-20 18:34:23 +0300

    Created Popular movies and top rated movies Api responses

--- app/src/main/java/com/example/chillmax/domain/models/responses/TopRatedMoviesApiResponses.kt
+++ app/src/main/java/com/example/chillmax/domain/models/responses/TopRatedMoviesApiResponses.kt
@@ -1,13 +1,15 @@
 package com.example.chillmax.domain.models.responses
 
 import com.example.chillmax.domain.models.PopularMovies
+import com.example.chillmax.domain.models.TopRatedMovies
 import kotlinx.serialization.Serializable
 
+
 @Serializable
-data class PopularMoviesApiResponses(
+data class TopRatedMoviesApiResponses(
     val success: Boolean,
     val message: String? = null,
     val prevPage: Int? = null,
     val nextPage: Int? =null,
-    val genres: List<PopularMovies> = emptyList()
+    val genres: List<TopRatedMovies> = emptyList()
 )
