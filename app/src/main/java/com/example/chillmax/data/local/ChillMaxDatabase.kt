ef0eec99c1bb2ead73a2ebfa852e83fbc845d4d5
Mzazi25 <langat.caleb25@gmail.com>
2022-09-20 11:14:26 +0300

    Created ChillMax Database

---
+++ app/src/main/java/com/example/chillmax/data/local/ChillMaxDatabase.kt
@@ -0,0 +1,20 @@
+package com.example.chillmax.data.local
+
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.chillmax.data.local.dao.*
+import com.example.chillmax.domain.models.*
+
+@Database(entities = [Genres::class, PopularMovies::class, TopRatedMovies::class, TVAiringToday::class, TVPopular::class, TVTopRated::class, UpcomingMovies::class], version = 1)
+@TypeConverters()
+abstract class ChillMaxDatabase: RoomDatabase(){
+    abstract fun genresDao(): GenresDao
+    abstract fun popularMoviesDao(): PopularMoviesDao
+    abstract fun topRatedMoviesDao(): TopRatedMoviesDao
+    abstract fun tvAiringTodayDao(): TVAiringTodayDao
+    abstract fun tvPopularDao(): TVPopularDao
+    abstract fun tvTopRatedDao(): TVTopRatedDao
+    abstract fun upcomingMoviesDao(): UpcomingMoviesDao
+}
