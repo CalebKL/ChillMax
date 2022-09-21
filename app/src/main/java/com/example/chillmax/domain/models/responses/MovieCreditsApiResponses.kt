ad28caa04ba7d2704f70b7d6a31944aff76ef349
Mzazi25 <langat.caleb25@gmail.com>
2022-09-21 09:03:02 +0300

    added Moviecredits API Responses

---
+++ app/src/main/java/com/example/chillmax/domain/models/responses/MovieCreditsApiResponses.kt
@@ -0,0 +1,13 @@
+package com.example.chillmax.domain.models.responses
+
+import com.example.chillmax.domain.models.MovieCredits
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MovieCreditsApiResponses(
+    val success: Boolean,
+    val message: String? = null,
+    val prevPage: Int? = null,
+    val nextPage: Int? =null,
+    val genres: List<MovieCredits> = emptyList()
+)
