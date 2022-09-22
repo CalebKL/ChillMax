576fb30028473b377844fdfceb5078967f8b4e2e
Mzazi25 <langat.caleb25@gmail.com>
2022-09-22 07:13:08 +0300

    created Network Module

---
+++ app/src/main/java/com/example/chillmax/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package com.example.chillmax.di
+
+import com.example.chillmax.data.remote.ChillMaxApi
+import com.example.chillmax.util.Constants.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.create
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient{
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit{
+        val contentType = MediaType.get("application/json")
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType = contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideChillMaxApi(retrofit: Retrofit): ChillMaxApi{
+        return retrofit.create(ChillMaxApi::class.java)
+    }
+}
