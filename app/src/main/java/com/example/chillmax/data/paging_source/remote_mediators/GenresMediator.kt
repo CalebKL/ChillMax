304ff07c63a8a7430f1a53cf8a97eb63dde6223f
Mzazi25 <langat.caleb25@gmail.com>
2022-09-22 06:30:50 +0300

    Added genres remote mediator

---
+++ app/src/main/java/com/example/chillmax/data/paging_source/remote_mediators/GenresMediator.kt
@@ -0,0 +1,25 @@
+package com.example.chillmax.data.paging_source.remote_mediators
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import com.example.chillmax.data.local.ChillMaxDatabase
+import com.example.chillmax.data.remote.ChillMaxApi
+import com.example.chillmax.domain.models.Genres
+import javax.inject.Inject
+
+@ExperimentalPagingApi
+class GenresMediator @Inject constructor(
+    private val chillMaxApi: ChillMaxApi,
+    private val chillMaxDatabase: ChillMaxDatabase
+): RemoteMediator<Int, Genres>() {
+    override suspend fun initialize(): InitializeAction {
+        return super.initialize()
+    }
+
+    override suspend fun load(loadType: LoadType, state: PagingState<Int, Genres>): MediatorResult {
+        TODO("Not yet implemented")
+    }
+
+}
