be4a922fd03fab3657545cd2d2beb464f39499c9
Mzazi25 <langat.caleb25@gmail.com>
2022-09-23 13:21:04 +0300

    Added all use cases in a single class

---
+++ app/src/main/java/com/example/chillmax/domain/use_cases/UseCases.kt
@@ -0,0 +1,20 @@
+package com.example.chillmax.domain.use_cases
+
+class UseCases(
+    val addToMyListUseCase: AddToMyListUseCase,
+    val deleteAllContentFromMyListUseCase: DeleteAllContentFromMyListUseCase,
+    val deleteOneFromMyListUseCase: DeleteOneFromMyListUseCase,
+    val getMovieCreditsUseCase: GetMovieCreditsUseCase,
+    val getMovieGenresUseCase: GetMovieGenresUseCase,
+    val getMyListUseCase: GetMyListUseCase,
+    val getPopularMoviesUseCase: GetPopularMoviesUseCase,
+    val getSelectedFromMyListUseCase: GetSelectedFromMyListUseCase,
+    val getTopRatedMoviesUseCase: GetTopRatedMoviesUseCase,
+    val getTVAiringTodayUseCase: GetTVAiringTodayUseCase,
+    val getTVCreditsUseCase: GetTVCreditsUseCase,
+    val getTVGenresUseCase: GetTVGenresUseCase,
+    val getTVPopularUseCase: GetTVPopularUseCase,
+    val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase,
+    val multiSearchUseCase: MultiSearchUseCase,
+    val tvTopRatedUseCase: TVTopRatedUseCase
+)
