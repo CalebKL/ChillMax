b89312a46212f4fec5bc4b8e5d7680cb5dd368d6
Mzazi25 <langat.caleb25@gmail.com>
2022-09-23 06:53:11 +0300

    Created TVAiringToday paging source

---
+++ app/src/main/java/com/example/chillmax/data/paging_source/TVAiringTodaySource.kt
@@ -0,0 +1,38 @@
+package com.example.chillmax.data.paging_source
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.chillmax.data.remote.ChillMaxApi
+import com.example.chillmax.domain.models.TVAiringToday
+import javax.inject.Inject
+
+class TVAiringTodaySource @Inject constructor(
+    private val chillMaxApi: ChillMaxApi
+): PagingSource<Int, TVAiringToday>() {
+
+    override fun getRefreshKey(state: PagingState<Int, TVAiringToday>): Int? {
+       return state.anchorPosition
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, TVAiringToday> {
+        return try {
+            val apiResponse = chillMaxApi.getTVAiringToday()
+            val tvAiringToday = apiResponse.tvAiringToday
+            if (tvAiringToday.isNotEmpty()) {
+                LoadResult.Page(
+                    data = tvAiringToday,
+                    prevKey = apiResponse.prevPage,
+                    nextKey = apiResponse.nextPage
+                )
+            } else {
+                LoadResult.Page(
+                    data = emptyList(),
+                    prevKey = null,
+                    nextKey = null
+                )
+            }
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
