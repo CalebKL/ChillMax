f772b289d927cc54d3cffde950a87c45cebd77fd
Mzazi25 <langat.caleb25@gmail.com>
2022-09-27 12:24:44 +0300

    Created welcome view model

--- app/src/main/java/com/example/chillmax/presentation/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/example/chillmax/presentation/welcome/WelcomeViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.chillmax.presentation.welcome
 
-class WelcomeViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chillmax.domain.use_cases.UseCases
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class WelcomeViewModel @Inject constructor(
+    private val useCases: UseCases
+): ViewModel() {
+    fun saveOnBoardingState(completed: Boolean){
+        viewModelScope.launch(Dispatchers.IO){
+            useCases.saveOnBoardingUseCase(completed = completed)
+        }
+    }
 }
