c528155c9164401549f27c5984dfc78dc2489b60
Mzazi25 <langat.caleb25@gmail.com>
2022-09-27 12:15:22 +0300

    created splash screen view model

--- app/src/main/java/com/example/chillmax/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/chillmax/presentation/splash/SplashViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.chillmax.presentation.splash
 
-class SplashViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chillmax.domain.use_cases.UseCases
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class SplashViewModel @Inject constructor(
+    private val useCases: UseCases
+):ViewModel() {
+    private val _onBoardingCompleted = MutableStateFlow(false)
+    val onBoardingCompleted: StateFlow<Boolean> = _onBoardingCompleted
+
+    init {
+        viewModelScope.launch(Dispatchers.IO){
+            _onBoardingCompleted.value =
+                useCases.readOnBoardingUseCase().stateIn(viewModelScope).value
+        }
+    }
 }
