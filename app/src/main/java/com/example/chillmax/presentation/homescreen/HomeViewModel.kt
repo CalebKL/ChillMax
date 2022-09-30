ab1f647044a1449b07cd3ed6b2888d8249e2b646
Mzazi25 <langat.caleb25@gmail.com>
2022-09-30 06:31:22 +0300

    Updated HomeviewModel

--- app/src/main/java/com/example/chillmax/presentation/homescreen/HomeViewModel.kt
+++ app/src/main/java/com/example/chillmax/presentation/homescreen/HomeViewModel.kt
@@ -9,5 +9,12 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val useCases: UseCases
 ): ViewModel() {
-
+    val getTVTopRatedUseCase = useCases.tvTopRatedUseCase
+    val getUpcomingMoviesUseCase = useCases.getUpcomingMoviesUseCase
+    val getTVPopularUseCase = useCases.getPopularMoviesUseCase
+    val getTVGenresUseCase = useCases.getTVGenresUseCase
+    val getTVAiringTodayUseCase = useCases.getTVAiringTodayUseCase
+    val getTopRatedMoviesUseCase = useCases.getTopRatedMoviesUseCase
+    val getPopularMoviesUseCase = useCases.getPopularMoviesUseCase
+    val getMovieGenresUseCase = useCases.getMovieGenresUseCase
 }
