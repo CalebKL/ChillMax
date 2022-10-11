65fdfb79e5e294f91f00338a8cd435467ff1fc18
Mzazi25 <langat.caleb25@gmail.com>
2022-10-11 12:04:47 +0300

    Updated Multisearch and tv airing model classes

--- app/src/main/java/com/example/chillmax/domain/models/TVAiringToday.kt
+++ app/src/main/java/com/example/chillmax/domain/models/TVAiringToday.kt
@@ -1,20 +1,33 @@
 package com.example.chillmax.domain.models
 
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
-@Serializable
 data class TVAiringToday(
+    @SerializedName("backdrop_path")
     val backdrop_path: String,
+    @SerializedName("first_air_date")
     val first_air_date: String,
+    @SerializedName("genre_ids")
     val genre_ids: List<Int>,
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("origin_country")
     val origin_country: List<String>,
+    @SerializedName("original_language")
     val original_language: String,
+    @SerializedName("original_name")
     val original_name: String,
+    @SerializedName("overview")
     val overview: String,
+    @SerializedName("popularity")
     val popularity: Double,
+    @SerializedName("poster_path")
     val poster_path: String,
+    @SerializedName("vote_average")
     val vote_average: Double,
+    @SerializedName("vote_count")
     val vote_count: Int
 )
