8b1be76d286dee35f33ed7e7b1471cdb95d0539f
Mzazi25 <langat.caleb25@gmail.com>
2022-10-12 10:23:53 +0300

    created bottom sheet scaffold state

--- app/src/main/java/com/example/chillmax/presentation/details/components/CastDetails.kt
+++ app/src/main/java/com/example/chillmax/presentation/details/components/CastDetails.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import com.example.chillmax.R
--- app/src/main/java/com/example/chillmax/presentation/details/components/DetailsContent.kt
+++ app/src/main/java/com/example/chillmax/presentation/details/components/DetailsContent.kt
@@ -1,22 +1,31 @@
 package com.example.chillmax.presentation.details.components
 
+import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.example.chillmax.R
 import com.example.chillmax.domain.models.MovieCredits
 import com.example.chillmax.domain.models.responses.MovieCreditsApiResponses
 import com.example.chillmax.presentation.ui.theme.EXTRA_SMALL_PADDING
+import com.example.chillmax.presentation.ui.theme.INFO_ICON_SIZE
 import com.example.chillmax.presentation.ui.theme.SHEET_PADDING
 import com.example.chillmax.presentation.ui.theme.SMALL_PADDING
 import com.example.chillmax.util.Resource
@@ -28,11 +37,12 @@ fun DetailsContent(
 
 }
 
+@ExperimentalCoilApi
 @Composable
 fun MovieBottomSheetContent(
     filmName:String,
     releaseDay: String,
-    casts: List<MovieCreditsApiResponses>,
+    casts: Resource<MovieCreditsApiResponses>,
     overview: String,
     sheetColor: Color = MaterialTheme.colors.surface,
     contentColor: Color = Color.LightGray
@@ -75,22 +85,75 @@ fun MovieBottomSheetContent(
             fontWeight = FontWeight.Medium,
             fontSize = 10.sp
         )
-
+        Spacer(modifier = Modifier.height(EXTRA_SMALL_PADDING))
+        if (casts is Resource.Success){
+            CastDetails(casts = casts.data!!)
+        }
     }
 }
 
+@ExperimentalCoilApi
 @Composable
-fun BackgroundColorSpan() {
-
+fun BackgroundColorSpan(
+    posterUrl: String,
+    imageFraction: Float = 1f,
+    backgroundColor:Color = MaterialTheme.colors.surface,
+    onCloseClick:()->Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = imageFraction + 0.4f)
+                .align(Alignment.TopStart),
+            painter = rememberImagePainter(
+                data = posterUrl,
+                builder = {
+                    placeholder(R.drawable.ic_placeholder)
+                    crossfade(true)
+                }
+            ),
+            contentScale = ContentScale.Crop,
+            contentDescription = stringResource(R.string.background_image)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ){
+            IconButton(
+                onClick = { onCloseClick()}
+            ) {
+                Icon(
+                    modifier= Modifier.size(INFO_ICON_SIZE),
+                    imageVector = Icons.Default.Close,
+                    contentDescription = stringResource(R.string.close_button),
+                    tint = Color.White
+                )
+            }
+        }
+    }
 }
 
-@Preview
-@Composable
-fun BottomSheetContentPreview() {
-    MovieBottomSheetContent(
-        filmName = "Vikings",
-        releaseDay = "20th July 2019",
-        overview = "kendenfkef rcrfnrfkrf",
-        casts = emptyList()
-    )
-}
+@ExperimentalMaterialApi
+val BottomSheetScaffoldState.currentSheetFraction:Float
+    get() {
+        val fraction = bottomSheetState.progress.fraction
+        val targetValue = bottomSheetState.targetValue
+        val currentValue = bottomSheetState.currentValue
+
+        Log.d("Fraction", fraction.toString())
+        Log.d("Target", targetValue.toString())
+        Log.d("Current", currentValue.toString())
+
+        return when{
+            currentValue == BottomSheetValue.Collapsed && targetValue == BottomSheetValue.Collapsed ->1f
+            currentValue == BottomSheetValue.Expanded && targetValue == BottomSheetValue.Expanded ->0f
+            currentValue == BottomSheetValue.Collapsed && targetValue == BottomSheetValue.Expanded ->1f - fraction
+            currentValue == BottomSheetValue.Expanded && targetValue == BottomSheetValue.Collapsed ->1f + fraction
+            else -> fraction
+        }
+    }
